6cdc932365f63b832bd73eddbc9bb7757534414e
Lukas Weibel <lukasweibel@air-von-lukas.zhaw.ch>
2024-04-23 08:12:58 +0200

    Initial commit

---
+++ src/main/java/ch/lukasweibel/powerlifting/PowerliftingApplication.java
@@ -0,0 +1,13 @@
+package ch.lukasweibel.powerlifting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerliftingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerliftingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/lukasweibel/powerlifting/PowerliftingApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.lukasweibel.powerlifting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerliftingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
